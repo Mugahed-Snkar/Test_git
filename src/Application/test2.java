41b0274c64577790875cd2644e3fee6dffdeee00
Mugahed Bin snkar <mugahed-bin-snkar@etud.univ-tln.fr>
2025-11-27 10:37:53 +0100

    commit

---
+++ src/Application/test2.java
@@ -0,0 +1,11 @@
+package Application;
+
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
+
+
+
--- src/test_git/testGit.java
+++ src/test_git/testGit.java
@@ -1,5 +1,11 @@
 package test_git;
 
 public class testGit {
+    public static void main(String[] args) {
+        System.out.println("Helloo");
+    }
 
 }
+
+
+
